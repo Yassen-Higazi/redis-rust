7cf48f79384ed0ba26d55bd29996523115f5bb51
Yassen.higazi.rasid <yassen.higazi@rasid.com.sa>
2024-10-06 12:01:33 +0300

    Ping pong

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
+use std::io::{Read, Write};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+
+                let mut buffer: Vec<u8> = vec![];
+
+                println!("Reading Data from socket...");
+
+                _stream.read_exact(&mut buffer).expect("Couldn't read");
+                
+                println!("Buffer: {buffer:?}");
+
+                _stream.write_all(b"+PONG\r\n").expect("Could not write to socket");
             }
 
             Err(e) => {
