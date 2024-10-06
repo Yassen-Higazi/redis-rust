d59ef35013fd6522312099ab8868006159413737
Yassen.higazi.rasid <yassen.higazi@rasid.com.sa>
2024-10-06 16:55:59 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,17 @@
 #![allow(unused_imports)]
-use anyhow::Context;
+use anyhow::{Context, Ok};
+use redis_server::listen;
 
 use std::io::{Read, Write};
 
-use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+
+mod redis_server;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-
-    loop {
-
-        let stream = listener.accept().await;
-
-        match stream {
-            Ok((mut stream, _)) => {
-                tokio::spawn(async move {
-                    let result = handle_connection(&mut stream).await.with_context(|| "Could not handle connection"); 
-
-                    match result {
-                        Ok(_) => {},
-
-                        Err(err) => {
-                            println!("Error: {:?}", err);
-                            stream.write_all(b"+Server Failure\r\n").await.with_context(|| format!("Error: {err:?}")).unwrap();
-                        }
-                    }
-                });
-            },
-
-            Err(e) => {
-                println!("error: {e:?}");
-                break;
-            }
-        };
-
-    }
-
-    Ok(())
-}
-
-
-async fn handle_connection(_stream: &mut TcpStream) -> anyhow::Result<()> {
-    println!("accepted new connection");
-
-    let mut buffer = [0u8; 512]; 
-
-    println!("Reading Data from socket...");
-
-    loop {
-        let bytes_read = _stream.read(&mut buffer).await?;
-
-        println!("Bytes read: {}", bytes_read);
-
-        if bytes_read == 0 {
-            break; 
-        }
-
-        let command = String::from_utf8(buffer[0..bytes_read].to_vec()).expect("Could not convert string");
-
-        println!("Command: {:?}", command);
-
-        _stream.write_all(b"+PONG\r\n").await?;
-    }
+    listen().await?;
 
     Ok(())
 }
