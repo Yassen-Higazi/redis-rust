e797b80037a42fb0e10152b9fa07a2912e60e37f
Yassen.higazi.rasid <yassen.higazi@rasid.com.sa>
2024-10-07 09:51:24 +0300

    refactor

--- src/redis_server.rs
+++ src/redis_server.rs
@@ -1,46 +1,49 @@
 use anyhow::Context;
-use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
 pub async fn listen() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
     loop {
-
         let stream = listener.accept().await;
 
         match stream {
             Ok((mut stream, _)) => {
                 tokio::spawn(async move {
-                    let result = handle_connection(&mut stream).await.with_context(|| "Could not handle connection"); 
+                    let result = handle_connection(&mut stream)
+                        .await
+                        .with_context(|| "Could not handle connection");
 
                     match result {
-                        Ok(_) => {},
+                        Ok(_) => {}
 
                         Err(err) => {
                             println!("Error: {:?}", err);
-                            stream.write_all(b"+Server Failure\r\n").await.with_context(|| format!("Error: {err:?}")).unwrap();
+                            stream
+                                .write_all(b"+Server Failure\r\n")
+                                .await
+                                .with_context(|| format!("Error: {err:?}"))
+                                .unwrap();
                         }
                     }
                 });
-            },
+            }
 
             Err(e) => {
                 println!("error: {e:?}");
                 break;
             }
         };
-
     }
 
     Ok(())
-
 }
 
 async fn handle_connection(_stream: &mut TcpStream) -> anyhow::Result<()> {
     println!("accepted new connection");
 
-    let mut buffer = [0u8; 512]; 
+    let mut buffer = [0u8; 512];
 
     println!("Reading Data from socket...");
 
@@ -50,10 +53,11 @@ async fn handle_connection(_stream: &mut TcpStream) -> anyhow::Result<()> {
         println!("Bytes read: {}", bytes_read);
 
         if bytes_read == 0 {
-            break; 
+            break;
         }
 
-        let command = String::from_utf8(buffer[0..bytes_read].to_vec()).expect("Could not convert string");
+        let command =
+            String::from_utf8(buffer[0..bytes_read].to_vec()).expect("Could not convert string");
 
         println!("Command: {:?}", command);
 
@@ -62,4 +66,3 @@ async fn handle_connection(_stream: &mut TcpStream) -> anyhow::Result<()> {
 
     Ok(())
 }
-
